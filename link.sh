db25f6a2431e2b5066ddd58c7ae1114d6050847b
Tri Nguyen <tri@tridnguyen.com>
2015-04-26 00:28:55 -0400

    sublime settings

--- link.sh
+++ link.sh
@@ -12,3 +12,15 @@ for file in link/{.,}*; do
 		ln -sfv `pwd`/$file ~/
 	fi
 done
+
+echo "Setting up Sublime Text settings..."
+for file in sublime/*.sublime-settings; do
+	sublimePath=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+	base=$(basename "$file")
+	echo $base
+	if [[ -f "$sublimePath"/"$base" ]]; then
+		echo "Skipping $base"
+		continue
+	fi
+	ln -sv $(pwd)/"$file" "$sublimePath"/"$base"
+done;
