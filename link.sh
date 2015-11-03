11dd2a5431e78a5c969d89fe4da532e3c6451a1c
Tri Nguyen <tri@tridnguyen.com>
2015-11-03 11:39:17 -0500

    [link] ignore .swp files

--- link.sh
+++ link.sh
@@ -6,18 +6,27 @@ function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
 
+# $1: directory path to link to
+# $2: file
+function do_link() {
+	base=$(basename "$2")
+	if [[ $2 -ef $1/$base ]]; then
+		e_arrow "Skipping $base"
+	else
+		ln -sfv "$2" "$1"/"$base"
+		e_success "Linking $base"
+	fi
+}
 # link file
 # $1: directory path to link to
 # $2: file
 function link_file() {
 	if [[ -f "$2" ]]; then
-		base=$(basename "$2")
-		if [[ $2 -ef $1/$base ]]; then
-			e_arrow "Skipping $base"
-		else
-			ln -sfv "$2" "$1"/"$base"
-			e_success "Linking $base"
-		fi
+		case "$2" in
+			# ignore .swp files
+			*.swp ) continue;;
+			* ) do_link "$1" "$2";;
+		esac
 	fi
 }
 
