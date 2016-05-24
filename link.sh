82aaa342e3e842cc31ad37796d2e6ff4504d57fb
Tri Nguyen <tri@tridnguyen.com>
2016-05-24 00:01:55 -0400

    link recursively in directory

--- link.sh
+++ link.sh
@@ -6,47 +6,69 @@ function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
 
-# $1: directory path to link to
-# $2: file
+# $1: file
+# $2: directory path to link to
 function do_link() {
-	base=$(basename "$2")
-	if [[ $2 -ef $1/$base ]]; then
+	base=$(basename "$1")
+	if [ "$1" -ef "$2/$base" ]; then
 		e_arrow "Skipping $base"
 	else
-		ln -sfv "$2" "$1"/"$base"
-		e_success "Linking $base"
+		ln -sfv "$1" "$2/$base"
+		if [ $? == 0 ]; then
+			e_success "Linked $base"
+		else
+			e_error "Failed to link $base to $2"
+		fi
 	fi
 }
 # link file
-# $1: directory path to link to
-# $2: file
+# $1: file
+# $2: directory path to link to
 function link_file() {
-	if [[ -f "$2" ]]; then
-		case "$2" in
+	if [ -f "$1" ]; then
+		case "$1" in
 			# ignore .swp files
-			*.swp ) continue;;
-			* ) do_link "$1" "$2";;
+			*.swp) 
+				continue
+				;;
+			*) 
+				do_link "$1" "$2"
+				;;
+		esac
+	elif [ -d "$1" ]; then
+		base=$(basename "$1")
+		case "$base" in
+			# skip current or prev directory
+			.|..)
+				continue
+				;;
+			# recursively call link_dir
+			*)
+				link_dir "$1" "$2/$base"
+				;;
 		esac
 	fi
 }
 
+# link dir
+# $1: directory to link
+# $2: directory to link to
+function link_dir() {
+	for file in $1/{.,}*; do
+		link_file "$file" "$2"
+	done
+}
 e_header "Setting up \$HOME dotfiles..."
-for file in $(pwd)/home/{.,}*; do
-	link_file "$HOME" "$file"
-done
+link_dir "$(pwd)/home" "$HOME"
 
 sublimePath=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-if [[ -d $sublimePath ]]; then
+if [ -d "$sublimePath" ]; then
 	e_header "Setting up Sublime Text settings..."
-	for file in $(pwd)/sublime/*.sublime-settings; do
-		link_file "$sublimePath" "$file"
-	done;
+	link_dir "$(pwd)/sublime" "$sublimePath"
 fi
 
 atomPath=~/.atom
-if [[ -d $atomPath ]]; then
+if [ -d "$atomPath" ]; then
 	e_header "Setting up Atom settings..."
-	for file in $(pwd)/atom/*; do
-		link_file "$atomPath" "$file"
-	done;
+	link_dir "$(pwd)/atom" "$atomPath"
 fi
