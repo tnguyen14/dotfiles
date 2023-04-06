b8486d9b30b948deccfb1d5483f9e7f4665a4f6d
Tri Nguyen <tri@tridnguyen.com>
2023-04-06 03:52:06 +0000

    safer get dir

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-$(dirname -- $0)/home/bin/lnk "$(dirname -- $0)/home" "$HOME"
+#!/bin/bash
+# see https://stackoverflow.com/a/1482133
+dir=$(dirname -- "$( readlink -f -- "$0";  )";)
+
+$dir/home/bin/lnk "$dir/home" "$HOME"
