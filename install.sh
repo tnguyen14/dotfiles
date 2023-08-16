89208758519b052099edf0051e724052032a47ae
Tri Nguyen <tri@tridnguyen.com>
2023-08-16 13:57:56 -0400

    Try installing tmux

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ $dir/home/bin/lnk "$dir/home" "$HOME"
 
 # source bashrc once to set up
 source "$HOME/.bashrc"
+
+# install tmux
+if ! command -v tmux &> /dev/null; then
+  mkdir -p "$HOME/github/tmux"
+  git clone https://github.com/tmux/tmux.git "$HOME/github/tmux/tmux"
+  cd "$HOME/github/tmux/tmux"
+  sh autogen.sh
+  ./configure
+  make && sudo make install
+fi
